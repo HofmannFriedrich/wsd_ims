4524d85d561dee5e7ef1d10975232c999813f1df
Aratz Puerto <aratzpuerto@gmail.com>
2016-06-13 18:27:39 +0200

    log for testing and training

--- test_all.sh
+++ test_all.sh
@@ -1,16 +1,18 @@
-# Train POS
-./test_POS.bash models/model_POS task17/test/lexical-sample/english-lexical-sample.test.xml tests/test_POS &
-# Train surr
-./test_surr.bash models/model_surr task17/test/lexical-sample/english-lexical-sample.test.xml tests/test_surr &
-#train coll
-./test_coll.bash models/model_coll task17/test/lexical-sample/english-lexical-sample.test.xml tests/test_coll &
-#train AWE
-./test_AWE.bash models/model_AWE task17/test/lexical-sample/english-lexical-sample.test.xml tests/test_AWE &
-#train CWE
-./test_CWE.bash models/model_CWE task17/test/lexical-sample/english-lexical-sample.test.xml tests/test_CWE &
-#train POS+surr+coll+AWE+CWE
-./test_c_POS+surr+coll+AWE+CWE.bash models/model_c_POS_surr_coll_AWE_CWE task17/test/lexical-sample/english-lexical-sample.test.xml tests/test_c_POS_surr_coll_AWE_CWE &
-#train POS+surr+coll+AWE
-./test_c_POS+surr+coll+AWE.bash models/model_model_c_POS_surr_coll_AWE task17/test/lexical-sample/english-lexical-sample.test.xml tests/test_c_POS_surr_coll_AWE &
-#train POS+surr+coll+CWE
-./test_c_POS+surr+coll+CWE.bash models/model_model_c_POS_surr_coll_CWE task17/test/lexical-sample/english-lexical-sample.test.xml tests/test_c_POS_surr_coll_CWE &
+#test POS
+./test_POS.bash models/model_POS task17/test/lexical-sample/english-lexical-sample.test.xml tests/test_POS > tests/test_POS.log 2>&1 &
+#test surr
+./test_surr.bash models/model_surr task17/test/lexical-sample/english-lexical-sample.test.xml tests/test_surr > tests/test_surr.log 2>&1 &
+#test coll
+./test_coll.bash models/model_coll task17/test/lexical-sample/english-lexical-sample.test.xml tests/test_coll > tests/test_coll.log 2>&1 &
+#test AWE
+./test_AWE.bash models/model_AWE task17/test/lexical-sample/english-lexical-sample.test.xml tests/test_AWE > tests/test_AWE.log 2>&1 &
+#test CWE
+./test_CWE.bash models/model_CWE task17/test/lexical-sample/english-lexical-sample.test.xml tests/test_CWE > tests/test_CWE.log 2>&1 &
+#test POS+surr+coll
+./test_c_POS+surr+coll.bash models/model_c_POS_surr_coll task17/test/lexical-sample/english-lexical-sample.test.xml tests/test_c_POS_surr_coll > tests/test_POS_surr_coll.log 2>&1 &
+#test POS+surr+coll+AWE+CWE
+./test_c_POS+surr+coll+AWE+CWE.bash models/model_c_POS_surr_coll_AWE_CWE task17/test/lexical-sample/english-lexical-sample.test.xml tests/test_c_POS_surr_coll_AWE_CWE  > tests/test_POS_surr_coll_AWE_CWE.log 2>&1 &
+#test POS+surr+coll+AWE
+./test_c_POS+surr+coll+AWE.bash models/model_model_c_POS_surr_coll_AWE task17/test/lexical-sample/english-lexical-sample.test.xml tests/test_c_POS_surr_coll_AWE > tests/test_POS_surr_coll_AWE.log 2>&1  &
+#test POS+surr+coll+CWE
+./test_c_POS+surr+coll+CWE.bash models/model_model_c_POS_surr_coll_CWE task17/test/lexical-sample/english-lexical-sample.test.xml tests/test_c_POS_surr_coll_CWE > tests/test_POS_surr_coll_CWE.log 2>&1  &
--- train_all.sh
+++ train_all.sh
@@ -1,16 +1,18 @@
 # Train POS
-./train_POS.bash task17/train/lexical-sample/english-lexical-sample.train.xml task17/train/lexical-sample/english-lexical-sample.train.key models/model_POS &
+./train_POS.bash task17/train/lexical-sample/english-lexical-sample.train.xml task17/train/lexical-sample/english-lexical-sample.train.key models/model_POS > models/model_POS.log 2>&1 &
 # Train surr
-./train_surr.bash task17/train/lexical-sample/english-lexical-sample.train.xml task17/train/lexical-sample/english-lexical-sample.train.key models/model_surr &
+./train_surr.bash task17/train/lexical-sample/english-lexical-sample.train.xml task17/train/lexical-sample/english-lexical-sample.train.key models/model_surr > models/model_surr.log 2>&1 &
 #train coll
-./train_coll.bash task17/train/lexical-sample/english-lexical-sample.train.xml task17/train/lexical-sample/english-lexical-sample.train.key models/model_coll &
+./train_coll.bash task17/train/lexical-sample/english-lexical-sample.train.xml task17/train/lexical-sample/english-lexical-sample.train.key models/model_coll > models/model_coll.log 2>&1 &
 #train AWE
-./train_AWE.bash task17/train/lexical-sample/english-lexical-sample.train.xml task17/train/lexical-sample/english-lexical-sample.train.key models/model_AWE &
+./train_AWE.bash task17/train/lexical-sample/english-lexical-sample.train.xml task17/train/lexical-sample/english-lexical-sample.train.key models/model_AWE > models/model_AWE.log 2>&1 &
 #train CWE
-./train_CWE.bash task17/train/lexical-sample/english-lexical-sample.train.xml task17/train/lexical-sample/english-lexical-sample.train.key models/model_CWE &
+./train_CWE.bash task17/train/lexical-sample/english-lexical-sample.train.xml task17/train/lexical-sample/english-lexical-sample.train.key models/model_CWE > models/model_CWE.log 2>&1 &
+#train POS+surr+coll
+./train_c_POS_surr_coll.bash task17/train/lexical-sample/english-lexical-sample.train.xml task17/train/lexical-sample/english-lexical-sample.train.key models/model_POS_surr_coll > models/model_POS_surr_coll.log 2>&1 &
 #train POS+surr+coll+AWE+CWE
-./train_POS+surr+coll+AWE+CWE task17/train/lexical-sample/english-lexical-sample.train.xml task17/train/lexical-sample/english-lexical-sample.train.key models/model_c_POS_surr_coll_AWE_CWE &
+./train_POS+surr+coll+AWE+CWE task17/train/lexical-sample/english-lexical-sample.train.xml task17/train/lexical-sample/english-lexical-sample.train.key models/model_c_POS_surr_coll_AWE_CWE > models/model_POS_surr_coll_AWE_CWE.log 2>&1 &
 #train POS+surr+coll+AWE
-./train_POS+surr+coll+AWE task17/train/lexical-sample/english-lexical-sample.train.xml task17/train/lexical-sample/english-lexical-sample.train.key models/model_c_POS_surr_coll_AWE &
+./train_POS+surr+coll+AWE task17/train/lexical-sample/english-lexical-sample.train.xml task17/train/lexical-sample/english-lexical-sample.train.key models/model_c_POS_surr_coll_AWE > models/model_POS_surr_coll_AWE.log 2>&1 &
 #train POS+surr+coll+CWE
-./train_POS+surr+coll+CWE task17/train/lexical-sample/english-lexical-sample.train.xml task17/train/lexical-sample/english-lexical-sample.train.key models/model_c_POS_surr_coll_CWE &
+./train_POS+surr+coll+CWE task17/train/lexical-sample/english-lexical-sample.train.xml task17/train/lexical-sample/english-lexical-sample.train.key models/model_c_POS_surr_coll_CWE > models/model_POS_surr_coll_CWE.log 2>&1 &
